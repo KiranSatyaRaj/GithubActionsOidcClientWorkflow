4b4ecf663e85ab823009822d963cb3a85585240d
KiranSatyaRaj <kiranjamy20021508@gmail.com>
2024-08-14 15:16:45 +0530

    sample push

--- src/main/java/ce/kiran/Main.java
+++ src/main/java/ce/kiran/Main.java
@@ -4,6 +4,7 @@ import dev.sigstore.KeylessSigner;
 import dev.sigstore.KeylessSignerException;
 import dev.sigstore.bundle.Bundle;
 import dev.sigstore.oidc.client.GithubActionsOidcClient;
+import dev.sigstore.oidc.client.OidcClients;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -15,11 +16,20 @@ import java.security.cert.CertificateException;
 import java.security.spec.InvalidKeySpecException;
 
 public class Main {
-    public static void main(String[] args) throws InvalidAlgorithmParameterException, CertificateException, IOException, NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, KeylessSignerException {
-        Path filePath = Paths.get("src/main/java/ce/kiran/hello.txt");
+    private static OidcClients retrieveOidcClients() {
         GithubActionsOidcClient client = GithubActionsOidcClient.builder().build();
-        KeylessSigner signer = new KeylessSigner.Builder().sigstorePublicDefaults().build();
-        Bundle result = signer.signFile(filePath);
-        System.out.println("Signature is " + result.getMessageSignature().get().getSignature());
+        return OidcClients.of(client);
+    }
+
+    private static Bundle signPayload() throws InvalidAlgorithmParameterException, CertificateException, IOException, NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, KeylessSignerException {
+        Path filePath = Paths.get("src/main/java/ce/kiran/hello.txt");
+        KeylessSigner signer = new SetDefaults().setOidcClients(retrieveOidcClients());
+        return signer.signFile(filePath);
+    }
+    public static void main(String[] args) throws InvalidAlgorithmParameterException, CertificateException, IOException, NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, KeylessSignerException {
+        Bundle result = signPayload();
+        OidcClients clients = retrieveOidcClients();
+        System.out.println("Oidc info: " + clients);
+        System.out.println("Signature is " + result.getCertPath().getCertificates().getFirst().toString());
     }
 }
--- src/main/java/ce/kiran/SetDefaults.java
+++ src/main/java/ce/kiran/SetDefaults.java
@@ -1,2 +1,34 @@
-package ce.kiran;public class SetDefaults {
+package ce.kiran;
+
+import dev.sigstore.KeylessSigner;
+import dev.sigstore.TrustedRootProvider;
+import dev.sigstore.encryption.signers.Signers;
+import dev.sigstore.fulcio.client.FulcioClient;
+import dev.sigstore.oidc.client.OidcClients;
+import dev.sigstore.rekor.client.RekorClient;
+import dev.sigstore.tuf.SigstoreTufClient;
+
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.spec.InvalidKeySpecException;
+
+public class SetDefaults {
+    private KeylessSigner.Builder builder = new KeylessSigner.Builder();
+
+    public KeylessSigner setOidcClients(OidcClients clients) throws InvalidAlgorithmParameterException, CertificateException, IOException, NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException {
+        this.builder.oidcClients(clients);
+        this.setDefaults();
+        return builder.build();
+    }
+
+    private void setDefaults() {
+        this.builder.signer(Signers.newEcdsaSigner())
+                .trustedRootProvider(TrustedRootProvider.from(SigstoreTufClient.builder().usePublicGoodInstance()))
+                .fulcioUrl(FulcioClient.PUBLIC_GOOD_URI)
+                .rekorUrl(RekorClient.PUBLIC_GOOD_URI)
+                .minSigningCertificateLifetime(KeylessSigner.DEFAULT_MIN_SIGNING_CERTIFICATE_LIFETIME);
+    }
 }
